95f86654cc3e3a152c795d1dec112f18503d6f35
Monica Hart <monica.hart@vmlyr.com>
2021-02-01 17:53:07 -0600

    adding deps and starting work creating a persistent class in Event.java

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -8,10 +11,12 @@ import java.util.Objects;
 /**
  * Created by Chris Bay
  */
+@Entity
 public class Event {
 
+    @Id /**I am the Primary key*/
+    @GeneratedValue /**tells database to generate the values of our primary key*/
     private int id;
-    private static int nextId = 1;
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -27,17 +32,13 @@ public class Event {
     private EventType type;
 
     public Event(String name, String description, String contactEmail, EventType type) {
-        this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
         this.type = type;
     }
-
-    public Event() {
-        this.id = nextId;
-        nextId++;
-    }
+/**will always need an empty constructor within a persistent or entity class*/
+    public Event() {}
 
     public String getName() {
         return name;
